11bff1c47e1b5fef93ffa4903d9df992bb9c1475
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-09-28 08:46:43 -0700

    Simplification by doing the xor to integers, not bits

--- set1/challenge2.py
+++ set1/challenge2.py
@@ -1,20 +1,6 @@
 # http://cryptopals.com/sets/1/challenges/2/
 from itertools import chain, imap, izip_longest, starmap
 
-def pad8(bits):
-    padding = 8 - len(bits)
-    return padding * [0] + bits
-
-
-def int2bits(integer):
-    result = []
-    div, mod = divmod(integer, 2)
-    result.append(mod)
-    while div:
-        div, mod = divmod(div, 2)
-        result.append(mod)
-    return pad8(list(reversed(result)))
-
 
 def hex2int(chars):
     return int(''.join(chars), 16)
@@ -23,36 +9,22 @@ def hex2int(chars):
 def hexdecode(input):
     args = [iter(input)] * 2
     hexdigits = izip_longest(fillvalue='', *args)
-    integers = imap(hex2int, hexdigits)
-    bytes_iter = imap(int2bits, integers)
-    return chain(*bytes_iter)
-
-
-def bits2int(bits):
-    size = len(bits) - 1
-    return sum([b * 2 ** (size - i) for i, b in enumerate(bits)])
+    return imap(hex2int, hexdigits)
 
 
 def int2hex(integer):
     return '%0.2x' % integer
 
 
-def hexencode(bits):
-    args = [iter(bits)] * 8
-    octects = izip_longest(fillvalue=0, *args)
-    integers = imap(bits2int, octects)
+def hexencode(integers):
     hexdigits = imap(int2hex, integers)
     return ''.join(hexdigits)
-
-
-def bitsxor(bit1, bit2):
-    return bit1 ^ bit2
 
 
 def hexxor(a, b):
     ad = hexdecode(a)
     bd = hexdecode(b)
-    result = starmap(bitsxor, izip_longest(ad, bd, fillvalue=0))
+    result = starmap(lambda a, b: a ^ b, izip_longest(ad, bd, fillvalue=0))
     return hexencode(result)
 
 
