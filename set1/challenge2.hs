3b38b1a7c2e123d838f1a19abd4095c55593900b
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-10-01 20:06:31 -0700

    Simplify Haskell solution by operating on integers, not bits

--- set1/challenge2.hs
+++ set1/challenge2.hs
@@ -1,4 +1,5 @@
 -- http://cryptopals.com/sets/1/challenges/2/
+import Data.Bits (xor)
 import Numeric (readHex)
 import Text.Printf (printf)
 
@@ -9,40 +10,20 @@ groupBy n xs = [take n xs] ++ groupBy n (drop n xs)
 hex2int :: [Char] -> Int
 hex2int x = fst  $ head  $ readHex x
 
-mods :: Int -> [Int]
-mods 0 = [0]
-mods i =
-    let (div, mod) = divMod i 2
-    in mod:mods div
-
-pad8 :: [Int] -> [Int]
-pad8 xs = if length(xs) < 8 then pad8 (0:xs) else xs
-
-int2bits :: Int -> [Int]
-int2bits i = pad8 $ reverse $ mods i
-
 hexDecode :: [Char] -> [Int]
-hexDecode input = concat [int2bits $ hex2int i | i <- groupBy 2 input]
-
-bitsXor :: Int -> Int -> Int
-bitsXor a b = if a == b then 0 else 1
-
-bits2int :: [Int] -> Int
-bits2int xs =
-    let size = (length xs) - 1
-    in sum $ zipWith (\i b -> b * 2 ^ (size - i)) [0..] xs
+hexDecode input = [hex2int i | i <- groupBy 2 input]
 
 int2hex :: Int -> [Char]
 int2hex i = printf "%02x" i
 
 hexEncode :: [Int] -> [Char]
-hexEncode input = concat [int2hex $ bits2int i | i <- groupBy 8 input]
+hexEncode input = concat [int2hex i | i <- input]
 
 hexXor :: [Char] -> [Char] -> [Char]
 hexXor input1 input2 =
     let decodedInput1 = hexDecode input1
         decodedInput2 = hexDecode input2
-    in hexEncode (zipWith bitsXor decodedInput1 decodedInput2)
+    in hexEncode (zipWith xor decodedInput1 decodedInput2)
 
 input1 = "1c0111001f010100061a024b53535009181c"
 input2 = "686974207468652062756c6c277320657965"
